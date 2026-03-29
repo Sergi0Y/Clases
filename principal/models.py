9928916ee425f813dbc570321fa9dd9054f3a7c6
Sergi0Y <ssergio.yanez@gmail.com>
2026-03-29 00:10:05 -0300

    docs: create db project (beta)

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    nombre = models.CharField(max_length=200)
+    profesor = models.CharField(max_length=200)
+    n_alumnos = models.IntegerField()
+    descripción = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return
